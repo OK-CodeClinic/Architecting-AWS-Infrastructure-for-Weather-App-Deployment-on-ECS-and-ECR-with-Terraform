f166b9efdf7aad686dde5122d45d7d111477df10
Kehinde Omokungbe <kehindefranklin@gmail.com>
2024-05-07 14:39:27 +0100

    Creating Load balancer and all its dependencies

---
+++ modules/alb/variables.tf
@@ -0,0 +1,11 @@
+variable "project_name" {}
+
+variable "alb_sg_id" {}
+
+variable "public_subnet_az1_id" {}
+
+variable "public_subnet_az2_id" {}
+
+variable "vpc_id" {}
+
+variable "certificate_arn" {}
